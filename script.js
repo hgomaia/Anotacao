d49903b215608968f51357931a4c9e13e96616f2
hugomaia2005 <40714146-hugomaia2005@users.noreply.replit.com>
2025-04-01 12:38:34 +0000

    Anotação 1/04

---
+++ script.js
@@ -0,0 +1,23 @@
+//Memória 
+var x
+const y 
+
+//Processador
++ //cuidado: + pode ser usado para juntar duas características, ou seja, 1 + 1 = 11, tem que usar algum dos parse
+- * / 
+% //resto de divisão 
+Math.pow(x,y) // potência 
+Math.sqrt(x) // raiz quadrada
+Math.pow(x,1/y) // outras raízes; ex: raiz cúbica de x = x^1/3
+
+//Entrada
+y = prompt("Olá, qual é o seu nome?")
+
+//Saída
+alert(x)
+alert("Olá, mundo!"+x)
+
+//Converter
+parseInt() //parseInt só aceita número inteiro
+parseFloat() //parseFloat só aceita número real
+Number() //Number aceita em qualquer formato
