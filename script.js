bce531b1166798f53275e0e9aad27c4d82f325f4
Hugo Maia <hugomaia2005@gmail.com>
2025-04-01 13:23:35 +0000

    Pequeno obs da anotação de 01/04

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const y
 + //cuidado: + pode ser usado para juntar duas características, ou seja, 1 + 1 = 11, tem que usar algum dos parse
 - * / 
 % //resto de divisão 
-Math.pow(x,y) // potência 
-Math.sqrt(x) // raiz quadrada
+Math.pow(x,y) // potência de x^y
+Math.sqrt(x) // raiz quadrada de x
 Math.pow(x,1/y) // outras raízes; ex: raiz cúbica de x = x^1/3
 
 //Entrada
