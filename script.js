2168337f4d64c240d4dc8f1a2c8fc714c45f70ea
Hugo Maia <hugomaia2005@gmail.com>
2025-04-10 20:02:21 +0000

    8/04 e 10/04

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-//Memória 
+//Memória (variáveis)
 var x
 const y 
 
-//Processador
-+ //cuidado: + pode ser usado para juntar duas características, ou seja, 1 + 1 = 11, tem que usar algum dos parse
-- * / 
+//Processador (operadores)
++ //cuidado: + pode ser usado para juntar duas características, ou seja, 1 + 1 = 11, para evitar use  algum dos parse
+- 
+* 
+** //potência (funciona em algumas células)
+/ 
 % //resto de divisão 
 Math.pow(x,y) // potência de x^y
 Math.sqrt(x) // raiz quadrada de x
 Math.pow(x,1/y) // outras raízes; ex: raiz cúbica de x = x^1/3
 
-//Entrada
+//Entrada - prompt, confirm
 y = prompt("Olá, qual é o seu nome?")
 
-//Saída
+//Saída - alert, console.log, document.write
 alert(x)
 alert("Olá, mundo!"+x)
 
-//Converter
+//Converter tipos de dados
 parseInt() //parseInt só aceita número inteiro
 parseFloat() //parseFloat só aceita número real
 Number() //Number aceita em qualquer formato
+
+//Decisão (if, else if, else)
+if (x > 6){
+  alert("x é maior que 6")
+} else if (x == 6){
+  alert("x é igual a 6";
+}else{
+  alert("x é menor que 6")
+}
+// if é uma condição, else é o que acontece se a condição não for atendida e else if é uma condição dentro de outra condição 
+//Operadores Relacionais
+> //maior
+< //menor
+>= </>//maior ou igual
+<= //menor ou igual
+== //igual
+!= //diferente de
+&& //e Ex: x > 6 && x < 10
+|| //ou
